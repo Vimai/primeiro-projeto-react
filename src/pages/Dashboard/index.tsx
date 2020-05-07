fb3852a58eb5d037f86af04122d8b5955bdb43b2
Vinicius Imai <vinicius.imai@gmail.com>
2020-05-06 23:23:16 -0300

    Adicionando tela do dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore diversos repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore diversos repositórios no GitHub</Title>
+
+      <Form>
+        <input
+          type="text"
+          placeholder="Digite aqui o seu melhor repositório!"
+        />
+        <button type="submit"> Pesquisar </button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/11447780?s=460&u=cb4370914fa15619100f47aee5c9b53e17a36cae&v=4"
+            alt="GitHub Explorer"
+          />
+          <div>
+            <strong>Vimai asd</strong>
+            <p> testestest </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/11447780?s=460&u=cb4370914fa15619100f47aee5c9b53e17a36cae&v=4"
+            alt="GitHub Explorer"
+          />
+          <div>
+            <strong>Vimai asd</strong>
+            <p> testestest </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
